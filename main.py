9fc8f4dd737f4e1aa731f388ea7de19a3835f62a
Jorge0929 <jorgeandresabino123@gmail.com>
2024-07-09 08:37:34 -0500

    Ejecución api basica
    Instalanción de las librerias y modificacion del main

---
+++ main.py
@@ -0,0 +1,18 @@
+from typing import Union
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get('/hola')
+def hola_mundo():
+    return {'Hola':'Mundo'}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
