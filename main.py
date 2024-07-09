db7a559d1f6882056f141310069e408180dd7410
Jorge0929 <jorgeandresabino123@gmail.com>
2024-07-09 17:58:50 -0500

    crear el modelo item con pydantic

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from typing import Union
-
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
 
 @app.get("/")
 def read_root():
@@ -16,3 +20,7 @@ def hola_mundo():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+@app.get('/calculadora')
+def calcular(operando_1:float, operando_2:float):
+    return {'suma': operando_1 + operando_2}
